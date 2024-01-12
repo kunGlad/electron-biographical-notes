4133a5c3125b3110a0e810ad60377cf32a23b847
zhaokun <1613430161@qq.com>
2024-01-12 17:11:22 +0800

    feat: (将redux 和model联系起来)

--- app/renderer/app.tsx
+++ app/renderer/app.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './router';
 
+import store from './store';
+import { Provider } from 'react-redux';
+
 function App() {
-  return <Router />;
+  // 我们将经过 createStore 生成的 store 挂载到 react-redux 提供的 Provider 组件上，这个 Provider 的工作任务是：通过 context 向子组件提供 store。
+  return (
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
