76d87948d234337e50bc6917c3c21410752eae5f
zhaokun <1613430161@qq.com>
2024-01-12 15:51:49 +0800

    feat: (首页开发+路由跳转)

--- app/renderer/app.tsx
+++ app/renderer/app.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Router from './router';
 
 function App() {
   return <Router />;
-  // return (
-  //   <Router>
-  //     <Switch>
-  //       <Route path="/">
-  //         <div>可视化简历平台</div>
-  //         <div>这是electron+react</div>
-  //       </Route>
-  //     </Switch>
-  //   </Router>
-  // );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- app/renderer/router.tsx
+++ app/renderer/router.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
-import Root from './container/root';
+import Root from '@src/container/root';
+import Resume from '@src/container/resume';
 
 function Router() {
   return (
     <HashRouter>
       <Switch>
         {/* 一定要添加exact */}
+
         <Route path="/" exact>
+          {/* 首页 */}
           <Root />
+        </Route>
+        <Route path="/resume" exact>
+          {/* 简历模块 */}
+          <Resume />
         </Route>
       </Switch>
       {/* 重定向到首页 */}
